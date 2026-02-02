847323c0ac5cbb336f3982f230881a1bd2bc15d5
GABIFABRO\fabro <fabrogabrielandres@gmail.com>
2026-02-02 14:33:24 +0100

    Implement message input and send button; integrate username hook and clipboard functionality

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -1,16 +1,23 @@
 
 'use client';
 
-import { useParams } from 'next/navigation';
-import { useState } from 'react';
+import { useUsername } from '@/hooks/use-username';
+import { useParams, useSearchParams, useRouter, usePathname } from 'next/navigation';
+import { useRef, useState } from 'react';
 
 export default function ComponentName() {
     const params = useParams();
     const roomId = params.roomId as string;
 
 
+    const router = useRouter()
+
+    const { username } = useUsername()
+    const [input, setInput] = useState("")
+    const inputRef = useRef<HTMLInputElement>(null)
 
     const [copyStatus, setCopyStatus] = useState("COPY")
+    const [timeRemaining, setTimeRemaining] = useState<number | null>(null)
 
 
 
@@ -21,6 +28,10 @@ export default function ComponentName() {
         setTimeout(() => setCopyStatus("COPY"), 2000)
     }
 
+    const sendMessage = () => {
+        // TODO: implement send message
+        setInput("")
+    }
 
 
     return (
@@ -41,7 +52,7 @@ export default function ComponentName() {
                     </div>
 
                     <div className="h-8 w-px bg-zinc-800" />
-
+      
                 </div>
 
                 <button
@@ -52,7 +63,35 @@ export default function ComponentName() {
                 </button>
             </header>
 
+            MESSAGES
+            <div className="flex-1 overflow-y-auto p-4 space-y-4 scrollbar-thin">
 
+            </div>
+
+            <div className="p-4 border-t border-zinc-800 bg-zinc-900/30">
+                <div className="flex gap-4">
+                    <div className="flex-1 relative group">
+                        <span className="absolute left-4 top-1/2 -translate-y-1/2 text-green-500 animate-pulse">
+                            {">"}
+                        </span>
+                        <input
+                            autoFocus
+                            type="text"
+                            value={input}
+                            placeholder="Type message..."
+                            onChange={(e) => setInput(e.target.value)}
+                            className="w-full bg-black border border-zinc-800 focus:border-zinc-700 focus:outline-none transition-colors text-zinc-100 placeholder:text-zinc-700 py-3 pl-8 pr-4 text-sm"
+                        />
+                    </div>
+
+                    <button
+            
+                        className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
+                    >
+                        SEND
+                    </button>
+                </div>
+            </div>
         </main>
     );
 }
